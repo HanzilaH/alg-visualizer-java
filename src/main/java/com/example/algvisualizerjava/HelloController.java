1e340abf4f384547f33ed8f20b6dcf1a4ab74b7f
HanzilaH <120693000+HanzilaH@users.noreply.github.com>
2023-08-14 04:15:43 +0100

    threads working although it doesnt stop automatically at the moment

--- src/main/java/com/example/algvisualizerjava/HelloController.java
+++ src/main/java/com/example/algvisualizerjava/HelloController.java
@@ -1,14 +1,117 @@
 package com.example.algvisualizerjava;
 
+import javafx.animation.*;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    public Text algorithmName;
+    public Text algorithmExplanation;
+    public HBox rectangleContainer;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private int NUMBER_OF_RECTANGLE = 50;
+    int RECTANGLE_WIDTH;
+
+    Thread sortingThread;
+
+
+    private int[] integerArray = new int[NUMBER_OF_RECTANGLE];
+
+
+    private Random random = new Random();
+
+
+
+
+    public void resetRectangleContainer(){
+//        if (sortingThread!=null){
+//            sortingThread.interrupt();
+//
+//        }
+        rectangleContainer.getChildren().clear();
+        RECTANGLE_WIDTH = (int)Math.round(rectangleContainer.getWidth()/ NUMBER_OF_RECTANGLE);
+        int lowerBound = 100;
+        int upperBound = 250;
+        int range = upperBound - lowerBound + 1;
+
+        for(int i = 0; i< NUMBER_OF_RECTANGLE; i++){
+            int randomNumber = random.nextInt(range) + lowerBound;
+            integerArray[i] = randomNumber;
+            Rectangle rect = new Rectangle(RECTANGLE_WIDTH, randomNumber );
+            rect.setFill(Color.LIGHTBLUE);
+            rectangleContainer.getChildren().add(rect);
+
+        }
+
+
+
     }
+    private void redrawRectangles(int[] integerArray, int current, int compareIndex) {
+        Platform.runLater(() -> {
+            rectangleContainer.getChildren().clear();
+            for (int i = 0; i < NUMBER_OF_RECTANGLE; i++) {
+                Rectangle rect = new Rectangle(RECTANGLE_WIDTH, integerArray[i]);
+                if (i == current) {
+                    rect.setFill(Color.RED);
+                } else if (i == compareIndex) {
+                    rect.setFill(Color.LIGHTGREEN);
+                } else {
+                    rect.setFill(Color.LIGHTBLUE);
+                }
+                rectangleContainer.getChildren().add(rect);
+            }
+        });
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+
+                redrawRectangles(array, i, j);
+
+            }
+            array[j + 1] = key;
+        }
+    }
+
+
+    public void startSorting() {
+        sortingThread = new Thread(() -> {
+            insertionSort(integerArray);
+
+        });
+
+        sortingThread.start();
+
+
+
+    }
+
+
 }
